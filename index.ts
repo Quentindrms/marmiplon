3587d3d5b8aee86464bb2c9d8526ff604529cabf
Quentin Derimais <quentin.derimais@gmail.com>
2025-07-29 15:30:17 +0200

    Création de la page index en EJS et des partials EJS Implémentation du globalRouter et du router index Appel de EJS dans index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { logMiddleware } from "./middlewares";
 import Express from "express";
+import router from "./routes/global";
 
 
 
@@ -8,10 +9,11 @@ const Port = 3000;
 
 
 app.use(Express.json());
-
 app.use(logMiddleware);
 
-
+/** Mise en place du moteur de rendu */
+app.set('view engine', 'ejs');
+app.use("/", router)
 
 app.listen(Port, () => {
   console.log(`Server is running on http://localhost:${Port}`);
---
+++ routes/globalRouter.ts
@@ -0,0 +1,9 @@
+import { request, Router } from "express";
+
+const globalRouter = Router();
+
+globalRouter.get('/', (request, response) => {
+    
+})
+
+export default globalRouter; 
