33d3fc542b6d548a40fdc6e19cca55cf238689c0
fouzy <chikouchefouzy@gmail.com>
2025-07-31 23:22:11 +0200

    get_date_by_id

--- index.ts
+++ index.ts
@@ -2,25 +2,23 @@ import { logMiddleware } from "./middlewares";
 import Express from "express";
 import router from "./routes/";
 import path from 'node:path'; 
-
+import { fileURLToPath } from 'node:url';
 
 const app = Express();
 const Port = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(Express.static(path.join(__dirname, 'public')));
 
 app.use(Express.json());
 app.use(logMiddleware);
 
-app.use("/", router)
-/** Mise en place du moteur de rendu */
-app.set("views", "./views/pages");
+app.set("views", path.join(__dirname, "views", "pages"));
 app.set('view engine', 'ejs');
 
-
-app.get("/recipe", (req, res) => {
-  res.render("recipePage");
-});
+app.use("/", router);
 
 app.listen(Port, () => {
   console.log(`Server is running on http://localhost:${Port}`);
